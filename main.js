c93b5496e7fd9b16345b8f36812bf88f61678b07
Kiyoshi <kiyoshin@mac.com>
2019-09-08 08:06:42 -0600

    Photoshop Image height, Image width, DPI, ICC Profile datas are from PSD Parser Addon.

--- main.js
+++ main.js
@@ -56,27 +56,7 @@ ipcMain.on("open-file-dialog", event => {
         event.sender.send("selected-directory", files);
         const filepath = files[0];
 
-        // Get XMP Data from xmpaddon.
-        const xmpString = xmpaddon.xmpRead(filepath);
-
-        // Parse from XMP String
-        const doc = new dom().parseFromString(xmpString);
-
-        // Extract "rdf:RDF" node
-        const rdfElement = doc.getElementsByTagName("rdf:RDF")[0];
-        const result = new serializer().serializeToString(rdfElement);
-
-        // Convert JSON String from XML
-        const jsonObj = convert.xml2js(result, {
-          compact: true,
-          spaces: 4
-        });
-        const metaObj = jsonObj["rdf:RDF"]["rdf:Description"];
-
-        // Find Creator Tool Informaiton
-        const creatorToolInfo = metaObj["xmp:CreatorTool"]["_text"];
-
-        console.log(creatorToolInfo);
+        const fileExtension = filepath.split(".").pop();
 
         var propertyDic = {};
 
@@ -86,59 +66,53 @@ ipcMain.on("open-file-dialog", event => {
 
         // Find Photoshop file or Illustrator file
 
-        if (creatorToolInfo.includes("Photoshop")) {
+        if (fileExtension == "psd" || fileExtension == "psb") {
           console.log("This is Photoshop File.");
+
+          const psdJSONString = PSDParserAddon.readPSD(filepath);
+
+          console.log(psdJSONString);
+
+          var obj = JSON.parse(psdJSONString);
 
           // Color Profile
 
-          if ("photoshop:ICCProfile" in metaObj) {
-            propertyDic["colorProfile"] =
-              metaObj["photoshop:ICCProfile"]["_text"];
+          if (obj.hasOwnProperty("ICC Profile")) {
+            propertyDic["colorProfile"] = obj["ICC Profile"];
           } else {
             propertyDic["colorProfile"] = "---";
           }
 
           // Document Width
 
-          if ("exif:PixelXDimension" in metaObj) {
-            propertyDic["width"] =
-              metaObj["exif:PixelXDimension"]["_text"] + " px";
+          if (obj.hasOwnProperty("width")) {
+            propertyDic["width"] = obj["width"] + " px";
           } else {
             propertyDic["width"] = "---";
           }
 
           // Document Height
 
-          if ("exif:PixelYDimension" in metaObj) {
-            propertyDic["height"] =
-              metaObj["exif:PixelYDimension"]["_text"] + " px";
+          if (obj.hasOwnProperty("height")) {
+            propertyDic["height"] = obj["height"] + " px";
           } else {
             propertyDic["height"] = "---";
           }
 
-          // DPI
+          // // DPI
 
-          if ("tiff:XResolution" in metaObj) {
-            const resolution = metaObj["tiff:XResolution"]["_text"];
-            const resolutionArr = resolution.split("/");
-            const resolutionNumFloat = parseFloat(resolutionArr[0]);
-            const resolutionDenFloat = parseFloat(resolutionArr[1]);
-            const dpiFloat = resolutionNumFloat / resolutionDenFloat;
-            propertyDic["dpi"] = dpiFloat.toString();
+          if (obj.hasOwnProperty("DPI")) {
+            propertyDic["dpi"] = obj["DPI"];
           } else {
             propertyDic["dpi"] = "---";
           }
 
           // Layers
 
-          const psdJSONString = PSDParserAddon.readPSD(filepath);
-          var obj = JSON.parse(psdJSONString);
           var layers = obj["layers"];
 
           var treelistNew =
-            '<ul class="fa-ul">' +
-            psdparse.createTreelistFromJSONObj(layers) +
-            "</ul>";
+            '<ul class="fa-ul">' + psdparse.createLayerlist(layers) + "</ul>";
 
           var psd = PSD.fromFile(filepath);
           psd.parse();
@@ -149,7 +123,24 @@ ipcMain.on("open-file-dialog", event => {
 
           var fonts = obj["fonts"];
           propertyDic["fonts"] = psdparse.createFontlist(fonts);
-        } else if (creatorToolInfo.includes("Illustrator")) {
+        } else {
+          // Get XMP Data from xmpaddon.
+          const xmpString = xmpaddon.xmpRead(filepath);
+
+          //Parse from XMP String
+          const doc = new dom().parseFromString(xmpString);
+
+          //Extract "rdf:RDF" node
+          const rdfElement = doc.getElementsByTagName("rdf:RDF")[0];
+          const result = new serializer().serializeToString(rdfElement);
+
+          //Convert JSON String from XML
+          const jsonObj = convert.xml2js(result, {
+            compact: true,
+            spaces: 4
+          });
+          const metaObj = jsonObj["rdf:RDF"]["rdf:Description"];
+
           console.log("This is illustrator File.");
 
           var unit = "";
--- src/PSDParse.js
+++ src/PSDParse.js
@@ -1,35 +1,11 @@
 var service = {
-  checkChildren: checkChildren,
-  createTreelist: createTreelist,
-  ctreatTextlist: ctreatTextlist,
-  createTreelistFromJSONObj: createTreelistFromJSONObj,
+  createLayerlist: createLayerlist,
   createFontlist: createFontlist
 };
 
 module.exports = service;
 
-function checkChildren(a, item) {
-  if (a.hasChildren()) {
-    a.children().forEach(function(node) {
-      var child;
-      if (node.hasChildren()) {
-        child = {
-          name: node.get("name"),
-          children: []
-        };
-      } else {
-        child = {
-          name: node.get("name")
-        };
-      }
-
-      item.children.push(child);
-      checkChildren(node, child);
-    });
-  }
-}
-
-function createTreelistFromJSONObj(a) {
+function createLayerlist(a) {
   var item = "";
 
   if (a["children"].length) {
@@ -41,37 +17,13 @@ function createTreelistFromJSONObj(a) {
           '<li><span class="fa-li"><i class="fas fa-folder"></i></span>' +
           node["name"] +
           '<ul class="fa-ul">' +
-          createTreelistFromJSONObj(node) +
+          createLayerlist(node) +
           "</ul></li>";
       } else {
         item =
           item +
           '<li><span class="fa-li"><i class="far fa-file"></i></span>' +
           node["name"] +
-          "</li>";
-      }
-    });
-  }
-  return item;
-}
-
-function createTreelist(a) {
-  var item = "";
-  if (a.hasChildren()) {
-    a.children().forEach(function(node) {
-      if (node.hasChildren()) {
-        item =
-          item +
-          '<li><span class="fa-li"><i class="fas fa-folder"></i></span>' +
-          node.get("name") +
-          '<ul class="fa-ul">' +
-          createTreelist(node) +
-          "</ul></li>";
-      } else {
-        item =
-          item +
-          '<li><span class="fa-li"><i class="far fa-file"></i></span>' +
-          node.get("name") +
           "</li>";
       }
     });
@@ -88,28 +40,6 @@ function createFontlist(a) {
       });
     }
   }
-
-  if (fontlist != "") {
-    "<ul>" + fontlist + "</ul>";
-  } else {
-    fontlist = "---";
-  }
-
-  return fontlist;
-}
-
-function ctreatTextlist(a) {
-  var type,
-    fonts = [],
-    fontlist = "";
-  a.descendants().forEach(function(node) {
-    type = node.get("typeTool");
-    if (!type) return;
-    fonts = fonts.concat(type.fonts()[0]);
-  });
-  fonts.forEach(function(node) {
-    fontlist = fontlist + "<li>" + node + "</li>";
-  });
 
   if (fontlist != "") {
     "<ul>" + fontlist + "</ul>";
